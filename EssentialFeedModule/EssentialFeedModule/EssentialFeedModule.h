0d2e0a478629b2707a1de2afc6b1225cc203bffd
HiramCM <91649588+HiramCM@users.noreply.github.com>
2021-10-28 16:04:53 -0700

    setting the right configuration for the project

---
+++ EssentialFeedModule/EssentialFeedModule/EssentialFeedModule.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedModule.h
+//  EssentialFeedModule
+//
+//  Created by Hiram Castro Maldonado on 28/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedModule.
+FOUNDATION_EXPORT double EssentialFeedModuleVersionNumber;
+
+//! Project version string for EssentialFeedModule.
+FOUNDATION_EXPORT const unsigned char EssentialFeedModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedModule/PublicHeader.h>
+
+
