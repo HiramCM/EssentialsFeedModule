10ea0eafed4f7df750d16e70f6a46857090bcc2f
HiramCM <91649588+HiramCM@users.noreply.github.com>
2021-10-28 18:33:31 -0700

    adding feed feature

---
+++ EssentialFeedModule/EssentialFeedModule/FeedFeature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeedModule
+//
+//  Created by Hiram Castro Maldonado on 28/10/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id:UUID
+    let description:String?
+    let location:String?
+    let imageURL:URL
+}
---
+++ EssentialFeedModule/EssentialFeedModule/FeedFeature/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeedModule
+//
+//  Created by Hiram Castro Maldonado on 28/10/21.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
