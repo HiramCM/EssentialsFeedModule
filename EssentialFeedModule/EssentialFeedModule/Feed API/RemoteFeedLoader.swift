39e39636e8ab49ec5359524bc91c9d444c919c0c
HiramCM <91649588+HiramCM@users.noreply.github.com>
2021-11-03 12:35:06 -0700

    delivers items array on 200 http response with json items

--- EssentialFeedModule/EssentialFeedModule/Feed API/RemoteFeedLoader.swift
+++ EssentialFeedModule/EssentialFeedModule/Feed API/RemoteFeedLoader.swift
@@ -46,8 +46,8 @@ public final class RemoteFeedLoader {
             switch result {
             case let .success(data, _):
                 
-                if let _ = try? JSONSerialization.jsonObject(with: data) {
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    completion(.success(root.items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -59,4 +59,8 @@ public final class RemoteFeedLoader {
             }
         }
     }
+}
+
+private struct Root: Decodable {
+    let items: [FeedItem]
 }
