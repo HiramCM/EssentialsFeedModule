4a8af1ff777d852bfacb0d4055655af269fd42e6
HiramCM <91649588+HiramCM@users.noreply.github.com>
2021-11-09 10:15:34 -0800

    validating that not deliver a result (invoke the completion closure) after the 'RemoteFeedLoader' instance has been deallocated

--- EssentialFeedModule/EssentialFeedModule/Feed API/RemoteFeedLoader.swift
+++ EssentialFeedModule/EssentialFeedModule/Feed API/RemoteFeedLoader.swift
@@ -30,13 +30,15 @@ public final class RemoteFeedLoader {
     public func load(completion: @escaping (Result) -> Void) {
         // 1 - calling the client get method simulating a httpRequest
         // waiting for the client completion handler to validate result response
-        client.get(from: url) { result in
+        client.get(from: url) { [weak self] result in
             // 4 - after execute the captured complition handler in HTTPClientSpy
             // the completion handler return to main caller (load) and get the result
             // failure or success and then executes main completion handler
+            guard self != nil else { return }
+            
             switch result {
             case let .success(data, response):
-                completion(RemoteFeedLoader.map(data, from: response))
+                completion(FeedItemsMapper.map(data, from: response))
                 break
             case .failure:
                 completion(.failure(.connectivity))
